4e730ba307936b1b5febaaf34a960a1a7c0d6f03
Eduardo Munari <munari.edu@gmail.com>
2022-07-13 12:43:36 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ FTP_USER = getenv("FTP_USER", 'user')  # pragma: no mutate
 FTP_PASSWORD = getenv("FTP_PASSWORD", 'pass')  # pragma: no mutate
 FTP_DIRECTORY = getenv("FTP_DIRECTORY", getcwd() + '/public/')  # pragma: no mutate
 FTP_PERM = getenv("FTP_PERM", 'elradfmw')  # pragma: no mutate
+FTP_TEST = "TESTE"
 
